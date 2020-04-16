ae547d33d44d8f2b12edee4de968e98967e1294e
Blanja Test 2 <53467582+blanjatest2@users.noreply.github.com>
2020-04-16 14:37:15 +0700

    Add script for important brew

---
+++ useful-brew.sh
@@ -0,0 +1,15 @@
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install cask #1
+brew install youtube-dl #2
+brew install speedtest-cli #3
+brew install imagemagick #4
+brew install mas #5
+brew install wifi-password #6
+brew install midnight-commander #7
+brew install zsh #8
+brew install htop #9
+brew install tree #10
+brew install archey #11
+brew install trash #12
+echo 'Source: https://medium.com/better-programming/12-terminal-tips-and-tricks-using-macos-and-homebrew-4e89c2ccb2fb'
